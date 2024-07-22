944338e6d78106693c5f9112130ec7eab47eb5da
SandaL-max <smartyalexpro@gmail.com>
2024-07-22 20:13:50 +0400

    multiple video streams in one sink

--- small_test.c
+++ small_test.c
@@ -1,38 +1,155 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <gst/gst.h>
+#include <glib.h>
+#include <signal.h>
 
-// Функция для конкатенации строки и числа
-char* concat_string_and_number(const char* str, int num) {
-    // Определяем длину строки и числа
-    size_t str_len = strlen(str);
-    char num_str[12]; // Максимальная длина int в десятичном представлении + знак
-    snprintf(num_str, sizeof(num_str), "%d", num);
-    size_t num_len = strlen(num_str);
+static GstElement *pipeline;
+static gboolean eos_received = FALSE;
 
-    // Выделяем память для результирующей строки
-    char* result = (char*)malloc(str_len + num_len + 1);
-    if (result == NULL) {
-        fprintf(stderr, "Ошибка выделения памяти\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Конкатенируем строку и число
-    strcpy(result, str);
-    strcat(result, num_str);
-
-    return result;
+// Signal handler for SIGINT to stop recording
+void sigint_handler(int sig) {
+    g_print("Received SIGINT, stopping...\n");
+    gst_element_send_event(GST_ELEMENT(pipeline), gst_event_new_eos());
 }
 
-int main() {
-    const char* my_string = "videoscale";
-    int my_number = 3;
+int main(int argc, char *argv[]) {
+    GstElement *source1, *source2, *videoscale1, *videoscale2, *convert1, *convert2, *queue1, *queue2, *compositor, *sink;
+    GstBus *bus;
+    GstMessage *msg;
+    GstStateChangeReturn ret;
+    GstCaps *caps1, *caps2;
 
-    char* result = concat_string_and_number(my_string, my_number);
-    printf("%s\n", result);
+    // Initialize GStreamer
+    gst_init(&argc, &argv);
 
-    // Освобождаем выделенную память
-    free(result);
+    // Create elements
+    source1 = gst_element_factory_make("ximagesrc", "source1");
+    videoscale1 = gst_element_factory_make("videoscale", "videoscale1");
+    convert1 = gst_element_factory_make("videoconvert", "convert1");
+    queue1 = gst_element_factory_make("queue", "queue1");
+
+    source2 = gst_element_factory_make("ximagesrc", "source2");
+    videoscale2 = gst_element_factory_make("videoscale", "videoscale2");
+    convert2 = gst_element_factory_make("videoconvert", "convert2");
+    queue2 = gst_element_factory_make("queue", "queue2");
+
+    compositor = gst_element_factory_make("compositor", "compositor");
+    sink = gst_element_factory_make("xvimagesink", "sink");
+
+    // Check that all elements are created successfully
+    if (!source1 || !videoscale1 || !convert1 || !queue1 ||
+        !source2 || !videoscale2 || !convert2 || !queue2 ||
+        !compositor || !sink) {
+        g_printerr("Failed to create one of the elements.\n");
+        return -1;
+    }
+
+    // Set properties for elements
+    g_object_set(source1, "startx", 0, "starty", 0, "use-damage", 0, NULL);
+    g_object_set(source2, "startx", 0, "starty", 0, "use-damage", 0, NULL);
+
+    // Create pipeline and add elements to it
+    pipeline = gst_pipeline_new("multi-screen-recorder");
+    gst_bin_add_many(GST_BIN(pipeline), source1, videoscale1, convert1, queue1,
+                     source2, videoscale2, convert2, queue2,
+                     compositor, sink, NULL);
+
+    // Set caps for videoscale
+    caps1 = gst_caps_new_simple("video/x-raw",
+                               "framerate", GST_TYPE_FRACTION, 5, 1,
+                               "width", G_TYPE_INT, 640,
+                               "height", G_TYPE_INT, 360,
+                               NULL);
+
+    caps2 = gst_caps_new_simple("video/x-raw",
+                               "framerate", GST_TYPE_FRACTION, 30, 1,
+                               "width", G_TYPE_INT, 1280,
+                               "height", G_TYPE_INT, 720,
+                               NULL);
+
+    // Link elements for source 1
+    if (!gst_element_link_many(source1, videoscale1, convert1, NULL) ||
+        !gst_element_link_filtered(convert1, queue1, caps1)) {
+        g_printerr("Failed to link elements for source 1.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Link elements for source 2
+    if (!gst_element_link_many(source2, videoscale2, convert2, NULL) ||
+        !gst_element_link_filtered(convert2, queue2, caps2)) {
+        g_printerr("Failed to link elements for source 2.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Set properties for compositor
+    GstPad *sinkpad1 = gst_element_request_pad_simple(compositor, "sink_%u");
+    GstPad *sinkpad2 = gst_element_request_pad_simple(compositor, "sink_%u");
+    g_object_set(sinkpad1, "xpos", 0, "ypos", 0, NULL);
+    g_object_set(sinkpad2, "xpos", 640, "ypos", 0, NULL);
+    gst_object_unref(sinkpad1);
+    gst_object_unref(sinkpad2);
+    //g_object_set(sink, "render-rectangle")
+
+    // Link compositor to sink
+    if (!gst_element_link(compositor, sink)) {
+        g_printerr("Failed to link compositor to sink.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Link queues to compositor
+    if (!gst_element_link(queue1, compositor) ||
+        !gst_element_link(queue2, compositor)) {
+        g_printerr("Failed to link queues to compositor.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Set the pipeline to the PLAYING state
+    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Failed to set pipeline to PLAYING state.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Register SIGINT handler
+    signal(SIGINT, sigint_handler);
+
+    // Wait for error or EOS messages
+    bus = gst_element_get_bus(pipeline);
+    while (!eos_received) {
+        msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                         GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+        if (msg != NULL) {
+            GError *err;
+            gchar *debug_info;
+
+            switch (GST_MESSAGE_TYPE(msg)) {
+                case GST_MESSAGE_ERROR:
+                    gst_message_parse_error(msg, &err, &debug_info);
+                    g_printerr("Error: %s\n", err->message);
+                    g_error_free(err);
+                    g_free(debug_info);
+                    break;
+                case GST_MESSAGE_EOS:
+                    g_print("End of stream reached.\n");
+                    eos_received = TRUE;
+                    break;
+                default:
+                    g_printerr("Unexpected message received.\n");
+                    break;
+            }
+            gst_message_unref(msg);
+        }
+    }
+
+    // Stop pipeline and release resources
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(bus);
+    gst_object_unref(pipeline);
 
     return 0;
 }
--- test.c
+++ test.c
@@ -1,9 +1,43 @@
+#include <gst/gst.h>
+#include <glib.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_LINE_LENGTH 100
 #define MAX_VIDEO_FORMATS 10
+
+static GstElement *pipeline;
+static gboolean eos_received = FALSE;
+
+// Signal handler for SIGINT to stop recording
+void sigint_handler(int sig) {
+    g_print("Received SIGINT, stopping...\n");
+    gst_element_send_event(GST_ELEMENT(pipeline), gst_event_new_eos());
+}
+
+// Функция для конкатенации строки и числа
+char* concat_string_and_number(const char* str, int video_format_count) {
+    // Определяем длину строки и числа
+    size_t str_len = strlen(str);
+    char num_str[12]; // Максимальная длина int в десятичном представлении + знак
+    snprintf(num_str, sizeof(num_str), "%d", video_format_count);
+    size_t num_len = strlen(num_str);
+
+    // Выделяем память для результирующей строки
+    char* result = (char*)malloc(str_len + num_len + 1);
+    if (result == NULL) {
+        fprintf(stderr, "Ошибка выделения памяти\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Конкатенируем строку и число
+    strcpy(result, str);
+    strcat(result, num_str);
+
+    return result;
+}
 
 // Структура для хранения параметров видеоформата
 typedef struct {
@@ -51,22 +85,170 @@ int parse_config_file(const char* filename, int* display_number, VideoFormat* vi
     return 0;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     const char* filename = "config.txt";
     int display_number;
     VideoFormat video_formats[MAX_VIDEO_FORMATS];
     int video_format_count;
 
     if (parse_config_file(filename, &display_number, video_formats, &video_format_count) != 0) {
-        fprintf(stderr, "Ошибка парсинга файла конфигурации.\n");
+        fprintf(stderr, "Error of parsing configuration file.\n");
         return EXIT_FAILURE;
     }
 
-    // Вывод результатов парсинга
-    printf("Display Number: %d\n", display_number);
-    for (int i = 0; i < video_format_count; i++) {
-        printf("Video Format %d: %d, %d, %d\n", i + 1, video_formats[i].width, video_formats[i].height, video_formats[i].framerate);
+    GstElement *source, *tee;
+    GstBus *bus;
+    GstMessage *msg;
+    GstStateChangeReturn ret;
+
+    // Initialize GStreamer
+    gst_init(&argc, &argv);
+
+    // Create elements
+    source = gst_element_factory_make("ximagesrc", "source");
+
+    GstElement *videoscales[video_format_count];
+    GstElement *converts[video_format_count];
+    GstElement *videorates[video_format_count];
+    GstElement *sink, *compositor;
+    GstElement *queues[video_format_count];
+    GstCaps *caps[video_format_count];
+
+    tee = gst_element_factory_make("tee", "tee");
+
+    for(int i = 0; i < video_format_count; i++) {
+        videoscales[i] = gst_element_factory_make("videoscale", concat_string_and_number("videoscale", i));
+        converts[i] = gst_element_factory_make("videoconvert", concat_string_and_number("convert", i));
+        videorates[i] = gst_element_factory_make("videorate", concat_string_and_number("videorate", i));
+        queues[i] = gst_element_factory_make("queue", concat_string_and_number("queue", i));
     }
+
+    compositor = gst_element_factory_make("compositor", "compositor");
+    sink = gst_element_factory_make("autovideosink", "sink");
+
+    // Check that all elements are created successfully
+    if (!source || !tee || !sink || !compositor) {
+        g_printerr("Failed to create one of the elements.\n");
+        return -1;
+    }
+    for (int i = 0; i < video_format_count; i++) {
+        if (!videoscales[i] || !converts[i] || !videorates[i] || !queues[i]) {
+            g_printerr("Failed to create one of the elements.\n");
+            return -1;
+        }
+    }
+
+    // Set properties for elements
+    g_object_set(source, "startx", 0, "use-damage", 0, NULL);
+
+    // Create pipeline and add elements to it
+    pipeline = gst_pipeline_new("multi-screen-recorder");
+    gst_bin_add_many(GST_BIN(pipeline), source, tee, NULL);
+    for (int i = 0; i < video_format_count; i++) {
+        gst_bin_add_many(GST_BIN(pipeline), queues[i], videoscales[i], converts[i], videorates[i], NULL);
+    }
+    gst_bin_add_many(GST_BIN(pipeline), compositor, sink, NULL);
+
+    // Set caps for videoscale
+
+    for (int i = 0; i < video_format_count; i++) {
+        caps[i] = gst_caps_new_simple("video/x-raw",
+                               "framerate", GST_TYPE_FRACTION, video_formats[i].framerate, 1,
+                               "width", G_TYPE_INT, video_formats[i].width,
+                               "height", G_TYPE_INT, video_formats[i].height,
+                               NULL);
+    }
+
+    // Link elements
+    if (!gst_element_link_many(source, tee, NULL)) {
+        g_printerr("Failed to link source to tee.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    for (int i = 0; i < video_format_count; i++) {
+        if (!gst_element_link_many(tee, queues[i], videorates[i], videoscales[i], NULL) ||
+            !gst_element_link_filtered(videoscales[i], converts[i], caps[i])) {
+            g_printerr("Failed to link elements for first sink.\n");
+            gst_object_unref(pipeline);
+            return -1;
+        }
+    }
+
+    for (int i = 0; i < video_format_count; i++) {
+        gst_caps_unref(caps[i]);
+    }
+
+    GstPad *sinkpads[video_format_count];
+    for (int i = 0; i < video_format_count; i++) {
+        sinkpads[i] = gst_element_request_pad_simple(compositor, "sink_%u");
+    }
+    int xpos_sum = 0;
+    for (int i = 0; i < video_format_count; i++) {
+        g_object_set(sinkpads[i], "xpos", xpos_sum, "ypos", 0, NULL);
+        xpos_sum += video_formats[i].width;
+    }
+    for (int i = 0; i < video_format_count; i++) {
+        gst_object_unref(sinkpads[i]);
+    }
+
+    if (!gst_element_link(compositor, sink)) {
+        g_printerr("Failed to link compositor to sink.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    for(int i = 0; i < video_format_count; i++) {
+        if (!gst_element_link(converts[i], compositor)) {
+            g_printerr("Failed to link queues to compositor.\n");
+            gst_object_unref(pipeline);
+            return -1;
+        }
+    }
+
+    // Set the pipeline to the PLAYING state
+    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Failed to set pipeline to PLAYING state.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    // Register SIGINT handler
+    signal(SIGINT, sigint_handler);
+
+    // Wait for error or EOS messages
+    bus = gst_element_get_bus(pipeline);
+    while (!eos_received) {
+        msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                         GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+    if (msg != NULL) {
+            GError *err;
+            gchar *debug_info;
+
+            switch (GST_MESSAGE_TYPE(msg)) {
+                case GST_MESSAGE_ERROR:
+                    gst_message_parse_error(msg, &err, &debug_info);
+                    g_printerr("Error: %s\n", err->message);
+                    g_error_free(err);
+                    g_free(debug_info);
+                    break;
+                case GST_MESSAGE_EOS:
+                    g_print("End of stream reached.\n");
+                    eos_received = TRUE;
+                    break;
+                default:
+                    g_printerr("Unexpected message received.\n");
+                    break;
+            }
+            gst_message_unref(msg);
+        }
+    }
+
+    // Stop pipeline and release resources
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(bus);
+    gst_object_unref(pipeline);
 
     return 0;
 }
